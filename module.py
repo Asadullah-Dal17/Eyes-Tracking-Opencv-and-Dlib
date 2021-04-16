4dbef82d0f0a943d43509f5b1abc3f4ef9cce442
Asadullah-Dal17 <aiphile17@gmail.com>
2021-04-16 11:55:38 +0500

    adding main.py and module.py

---
+++ module.py
@@ -0,0 +1,62 @@
+import cv2 as cv
+import numpy as np
+import dlib
+import math
+
+# variables
+fonts = cv.FONT_HERSHEY_COMPLEX
+# colors
+YELLOW = (0, 247, 255)
+CYAN = (255, 255, 0)
+MAGENTA = (255, 0, 242)
+GOLDEN = (32, 218, 165)
+LIGHT_BLUE = (255, 9, 2)
+PURPLE = (128, 0, 128)
+CHOCOLATE = (30, 105, 210)
+PINK = (147, 20, 255)
+ORANGE = (0, 69, 255)
+GREEN = (0, 255, 0)
+LIGHT_GREEN = (0, 255, 13)
+LIGHT_CYAN = (255, 204, 0)
+BLUE = (255, 0, 0)
+RED = (0, 0, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+LIGHT_RED = (2, 53, 255)
+
+
+# face detector object
+
+# landmarks detector
+
+
+# function
+
+
+def midpoint(pts1, pts2):
+    x, y = pts1
+    x1, y1 = pts2
+    xOut = int((x + x1)/2)
+    yOut = int((y1 + y)/2)
+    # print(xOut, x, x1)
+    return (xOut, yOut)
+
+
+def eucaldainDistance(pts1, pts2):
+    x, y = pts1
+    x1, y1 = pts2
+    eucaldainDist = math.sqrt((x1 - x) ** 2 + (y1 - y) ** 2)
+
+    return eucaldainDist
+
+
+def faceDetector():
+    pass
+
+
+def facePoint():
+    pass
+
+
+def blinkDetector():
+    pass
